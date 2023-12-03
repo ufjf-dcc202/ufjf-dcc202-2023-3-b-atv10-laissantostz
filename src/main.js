fe8ea0ed9257d5a25f7924b679f8871b7d074441
Laís Santos <104094721+LaisSantosIngage@users.noreply.github.com>
2023-12-03 16:59:54 -0300

    adiciona conteúdo no main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+import { getJoao, getMaria } from '../joaoEMaria.js';
+
 // main.js
 console.log('módulo main');
+
+console.log('Joao tem', getJoao(), ' maçãs!');
+console.log('Maria tem', getMaria(), 'maçãs!');
