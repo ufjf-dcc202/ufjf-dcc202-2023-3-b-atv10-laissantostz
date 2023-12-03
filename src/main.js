eb7b23b0e61407ea0f4b72226375dbece6725473
Laís Santos <104094721+LaisSantosIngage@users.noreply.github.com>
2023-12-03 17:11:03 -0300

    corrige conteúdo main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import { getJoao, getMaria } from '../joaoEMaria.js';
 // main.js
 console.log('módulo main');
 
-console.log('Joao tem', getJoao(), ' maçãs!');
+console.log('Joao tem', getJoao(), 'maçãs!');
 console.log('Maria tem', getMaria(), 'maçãs!');
